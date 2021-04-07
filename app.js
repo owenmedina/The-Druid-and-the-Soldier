53f76d255f7575e233dd071fc7e44792e1756a05
Owen Medina <owensmedi@gmail.com>
2021-04-07 14:56:14 -0700

    First commit - setup project

---
+++ app.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+app.use(express.static(`${__dirname}/public`));
+
+const port = parseInt(process.env.APP_PORT_NUMBER);
+
+app.get("/", function (req, res) {
+  console.log("At /");
+  res.sendFile(`${__dirname}/views/index.html`);
+});
+
+app.listen(port, function () {
+  console.log(
+    `Successfully setup server on port ${process.env.APP_PORT_NUMBER}`
+  );
+});
