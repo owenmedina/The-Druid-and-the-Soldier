0514e2f0eee76cdc930e837453926117c14e0a07
Owen Medina <owensmedi@gmail.com>
2021-04-08 12:34:59 -0700

    Create live reload functionality for front-end and back-end

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 require("dotenv").config();
 
+const path = require("path");
 const express = require("express");
-const app = express();
+const liveReload = require("livereload"); // Reloading browser when public/ files change
+const connectLiveReload = require("connect-livereload"); // Appends script on alll the rendered/sent files through the server's response object
 
-app.set("view engine", "ejs");
+const liveReloadServer = liveReload.createServer(); // Creates a server that listens for changes
+liveReloadServer.watch(path.join(__dirname, "public")); // Watches changes in the public folder
+// nodemon kills connection of live reload server as well every time the backend changes
+// reloads the page once the connection is established again on the live reload server and after the handshake of the live reload server (100ms timeout)
+liveReloadServer.server.once("connection", function () {
+  setTimeout(function () {
+    liveReloadServer.refresh("/"); // refreshes the page
+  }, 100);
+});
+
+const app = express();
+app.use(connectLiveReload()); // tells express to use the module (line must be before static and dynamic routes)
 app.use(express.static(`${__dirname}/public`));
+app.set("view engine", "ejs");
 
 const port = parseInt(process.env.APP_PORT_NUMBER);
 
 app.get("/", function (req, res) {
-  console.log("At /");
-  res.sendFile(`${__dirname}/views/index.html`);
+  console.log("At the /");
+  res.render("index", {});
 });
 
 app.listen(port, function () {
